4e663b8a775742da7925e7dd50ef567365eba199
Kalu Kalu <kalu@kaluekalu.com>
2018-05-02 17:44:26 +0800

    first

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from . models import Comment
+
+
+def home_page(request):
+    if request.method == 'POST':
+        our_comment = request.POST.get('comment')
+        our_user = request.POST.get("username")
+        comment_obj = Comment.objects.create(
+            comment=our_comment,
+            user=our_user
+        )
+        all_comments = Comment.objects.all()
+        context = {'comments': all_comments}
+
+        return render(request, 'blog/home_page.html', context)
+    if request.method == "GET":
+        all_comments = Comment.objects.all()
+        context = {'comments': all_comments}   
+        return render(request, 'blog/home_page.html', context)
